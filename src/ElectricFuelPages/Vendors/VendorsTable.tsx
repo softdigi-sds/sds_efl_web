ec456a4d8ec6425f6169151bbe1137bfefb2beec
pstamizh <pstamizhselvan@gmail.com>
2024-09-25 17:16:36 +0530

    update

--- src/ElectricFuelPages/Vendors/VendorsTable.tsx
+++ src/ElectricFuelPages/Vendors/VendorsTable.tsx
@@ -1,10 +1,130 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { SmartSoftTable, SmartTableNewInterface } from '../../core';
+import { get } from '../../services/smartApiService';
 
 const VendorsTable = () => {
+  const [data, setData] = useState([]);
+
+  const loadTableData = () => {   
+    const subscription = get("users").subscribe((response) => {
+      setData(response.data.users);    
+    });
+    return () => {
+      subscription.unsubscribe();
+    };
+  };
+
+  useEffect(() => {   
+    loadTableData();
+  }, []);
+
+  const openOfficesForm =()=>{
+    // let modelObject = {
+    //   body: (
+    //     <HubsForms
+         
+    //       closeModal={closeModal}
+    //     />
+    //   ),
+    //   modelClass: "customer-model-layout smart-modal-90",
+    //   bodyClose: false,
+    // };
+    // openModal(modelObject);
+  }
+  const handleDelete = (rowData: any) => {
+
+    console.log('Delete action for row:', rowData);
+  }
+
+  const buttons = [
+    {
+      label: "",
+      type: "icon",
+      leftIcon: "fa fa-eye",
+      classList: ["delete-color is-clickable is-size-5"],
+      onClick: handleDelete
+    },
+    {
+      label: "",
+      type: "icon",
+      leftIcon: " fa-pencil-square-o",
+      classList: ["delete-color is-clickable is-size-5"],
+      onClick: handleDelete
+    },
+    {
+      label: "",
+      type: "icon",
+      leftIcon: "fa fa-times",
+      classList: ["delete-color is-clickable is-size-5"],
+      onClick: handleDelete
+    },
+  ];
+  const columns: SmartTableNewInterface.SmartTableNewColumnConfig[] = [
+    { title: "S.NO", index: "s_no", type: "sno" },
+    {
+      title: "Hun Id",
+      index: "firstName",
+    },
+    {
+      title: "Code",
+      index: "lastName",
+    },
+    { title: "Company", index: "age" },
+    {
+      title: "Name",
+      index: "gender",
+    },
+    { title: "GST No", index: "email" },
+    { title: "Status", index: "email" },
+    {
+      title: "Action",
+      index: "action",
+      type: "buttons",
+      buttons: buttons,
+      width: "10",
+    },
+  ];
+  const tableTop: SmartTableNewInterface.SmartTableNewTopProps[] = [
+    {
+      type: "CUSTOM",
+      widthClass: "is-10",
+      custom: <p className="is-size-4">Vendors</p>,
+    },
+    {
+      type: "BUTTONS",
+      widthClass: "is-2",
+      align: "RIGHT",
+      buttons: [
+        
+        {
+          label:"Add",
+          icon:"fa-plus",
+          type:"CUSTOM",
+          action: openOfficesForm,
+        },
+      ],
+      
+    },
+   
+   
+    
+  ]
+
   return (
-    <div>
-      Vendors Table
-    </div>
+    <>
+      <SmartSoftTable
+        columns={columns}
+        data={data}
+        tableTop={tableTop}
+        tableProps={{
+          className: "table is-hoverable is-bordered is-striped smart-basic-pdf-table",
+          isResponsive: true,
+        }}
+        paginationProps={{
+          pageSize:5
+        }}
+      />
+      </>
   )
 }
 
