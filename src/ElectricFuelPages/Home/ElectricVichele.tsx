e0c7125559c783fe8803f95e6b1451bd3563538a
pstamizh <pstamizhselvan@gmail.com>
2024-09-25 14:21:12 +0530

    update

--- src/ElectricFuelPages/Home/ElectricVichele.tsx
+++ src/ElectricFuelPages/Home/ElectricVichele.tsx
@@ -18,11 +18,11 @@ const ElectricVichele = () => {
             <div className="columns">
               <div className="column is-4">
                 <div className="smart-elecric-vichele-card">
-                  <h2>
-                    <span>1</span>
+                  <h2 className="is-size-5">
+                    <span className="">1</span>
                     No fuel, no emissions
                   </h2>
-                  <p>
+                  <p className="is-size-6">
                     Reducing the carbon footprint is the need of the hour. The
                     electric engine within an EV operates on a closed circuit,
                     so an electric vehicle does not emit any of the gases often
@@ -30,22 +30,22 @@ const ElectricVichele = () => {
                   </p>
                 </div>
                 <div className="smart-elecric-vichele-card">
-                  <h2>
+                  <h2 className="is-size-5">
                     <span>2</span>
                     Low maintenance
                   </h2>
-                  <p>
+                  <p className="is-size-6">
                     Electric vehicles don’t require any expensive maintenance
                     like the fuel-run vehicles. Servicing is easy, less frequent
                     and overall cheaper.
                   </p>
                 </div>
                 <div className="smart-elecric-vichele-card">
-                  <h2>
+                  <h2 className="is-size-5">
                     <span>3</span>
                     Noise-free driving experience
                   </h2>
-                  <p>
+                  <p className="is-size-6">
                     The quietness of the vehicle creates a far more noise-free,
                     comfortable & relaxing driving experience.
                   </p>
@@ -58,32 +58,32 @@ const ElectricVichele = () => {
               </div>
               <div className="column is-4">
                 <div className="smart-elecric-vichele-card">
-                  <h2>
+                <h2 className="is-size-5">
                     <span>4</span>
                     Fast & convenient charging
                   </h2>
-                  <p>
+                  <p className="is-size-6">
                     EFL deploys ultrafast chargers at convenient locations to
                     speed up the charging process.
                   </p>
                 </div>
                 <div className="smart-elecric-vichele-card">
-                  <h2>
+                <h2 className="is-size-5">
                     <span>5</span>
                     Negligible running costs
                   </h2>
-                  <p>
+                  <p className="is-size-6">
                     The cost of the electricity required to charge an EV is
                     around 40% less than the cost to use petrol for a similar
                     sized vehicle driving the same distance.
                   </p>
                 </div>
                 <div className="smart-elecric-vichele-card">
-                  <h2>
+                <h2 className="is-size-5">
                     <span>6</span>
                     Freedom from rising fuel costs
                   </h2>
-                  <p>
+                  <p className="is-size-6">
                     With petrol and diesel prices at an all time high, shifting
                     to an EV saves us from the rising fuel costs. This makes
                     EV’s affordable in the long run too!
