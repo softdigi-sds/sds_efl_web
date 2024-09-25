0ce84d84b16067d88bc085d8264cc4dbd5d0dc78
pstamizh <pstamizhselvan@gmail.com>
2024-09-25 16:31:30 +0530

    update

--- src/ElectricFuelPages/Home/CmsPlateForm.tsx
+++ src/ElectricFuelPages/Home/CmsPlateForm.tsx
@@ -8,12 +8,12 @@ const CmsPlateForm = () => {
         <div className="row">
           <div className="columns">
             <div className="column is-5">
-              <div className="is-size-2">
+              <div className="is-size-4">
                 <span className="eletric-home-page-words">
                   Cloud based CMS platform
                 </span>
               </div>
-              <p className="m-0 is-size-5 mt-3">
+              <p className="m-0 mt-3">
                 Cloud based Central Management System (CMS), is a platform for
                 centralized monitoring and operations. This system will not only
                 ensure the highest levels of Charger quality and service
@@ -21,7 +21,7 @@ const CmsPlateForm = () => {
                 for seamless Authentication, Authorization and Payment process
                 in a full automated mode.
               </p>
-              <div className="mt-3 is-size-5">
+              <div className="mt-3">
                 <p>We are a full scale CMS Platform</p>
               </div>
             </div>
