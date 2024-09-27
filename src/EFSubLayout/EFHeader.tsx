d3caea509c0c0f0c085f9c32333f2524e8d548ea
Raju10136 <108601999+Raju10136@users.noreply.github.com>
2024-09-27 15:07:29 +0530

    ok

--- src/EFSubLayout/EFHeader.tsx
+++ src/EFSubLayout/EFHeader.tsx
@@ -5,14 +5,14 @@ import { useSiteContext } from '../contexts/SiteProvider';
 
 
 interface HeaderProps {
-  setIsOpen: (open:boolean) => void;
+  setIsOpen: (open: boolean) => void;
   isOpen: boolean
 }
 const EFHeader: React.FC<HeaderProps> = ({ setIsOpen, isOpen }) => {
   const navigate = useNavigate();
   const basenav = "/";
   const { user, setUser, logout, setLoading } = useSiteContext();
-  console.log("user " , user);
+  console.log("user ", user);
   const navigateLink = (index: any) => {
     // navigate(basenav + "/" + index);
     navigate(basenav + index);
@@ -22,71 +22,6 @@ const EFHeader: React.FC<HeaderProps> = ({ setIsOpen, isOpen }) => {
     navigateLink("")
   };
 
-  const HeaderProfile = () => {
-
-    return (
-      <>
-        <div className="ml-3 profile-button-dropdown dropdown   is-hoverable  pb-0 mt-1 mb-1 ">
-          <button className=" crop-business-laytout-top-button is-flex profile-hi-message-hover ">
-            <div className="business-profile-image  ">
-            </div>
-            <div className="has-text-weight-bold ml-2 mt-1 is-flex ">
-              <div>Hi Admin</div>
-              <i className="fa fa-sort-desc ml-1 is-size-6 " aria-hidden="true"></i>
-            </div>
-          </button>
-          <div className="dropdown-menu smart-header-dropdown-menu mr-6">
-            <div className=" dropdown-content-profile pb-3 ">
-              <div className="dropdown-item p-0">
-                <div className="columns mb-0">
-
-
-                </div>
-
-
-
-                <div className="mt-3" key="g9">
-                  <ul className=" has-text-center profile-business-icon-page px-2">
-                    <li
-                      className="smart-header-dropdown-list pointer"
-                      onClick={() => navigateLink("home/my-profile-form")}
-                    >
-                      <span className="customer-home-icon-text-color">
-                        <i
-                          className="fa fa-user-circle-o"
-                          aria-hidden="true"
-                        ></i>
-                      </span>
-                      <span>My Profile</span>
-                    </li>
-
-                    <li
-                      className="smart-header-dropdown-list pointer"
-                      onClick={() => navigateLink("settings/pin-change-form")}
-                    >
-                      <span className="customer-home-icon-text-color">
-                        <i className="fa fa-cog" aria-hidden="true"></i>
-                      </span>
-                      <span>Settings</span>
-                    </li>
-                    <li
-                      className="smart-header-dropdown-list pointer"
-                      onClick={handleLogout}
-                    >
-                      <span className="customer-home-icon-text-color">
-                        <i className="fa fa-sign-out" aria-hidden="true"></i>
-                      </span>
-                      <span>Logout</span>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </>
-    );
-  };
 
 
   // return (
@@ -123,7 +58,7 @@ const EFHeader: React.FC<HeaderProps> = ({ setIsOpen, isOpen }) => {
 
       <div id="navbarBasicExample" className="navbar-menu">
         <div className="navbar-start">
-          <a onClick={()=>setIsOpen(!isOpen)}className="navbar-item">
+          <a onClick={() => setIsOpen(!isOpen)} className="navbar-item">
             <i className='fa fa-bars'></i>
           </a>
         </div>
@@ -131,7 +66,7 @@ const EFHeader: React.FC<HeaderProps> = ({ setIsOpen, isOpen }) => {
         <div className="navbar-end">
           <div className="navbar-item has-dropdown is-hoverable ">
             <a className="navbar-link">
-                {user?.ename}
+              {user?.ename}
             </a>
 
             <div className="navbar-dropdown is-right">
@@ -141,7 +76,7 @@ const EFHeader: React.FC<HeaderProps> = ({ setIsOpen, isOpen }) => {
                 </span> Profile
               </a>
               <hr className="navbar-divider" />
-              <a className="navbar-item">
+              <a onClick={() => handleLogout()} className="navbar-item">
                 <span className='icon mr-1'>
                   <i className='fa fa-sign-out'></i>
                 </span>                Logout
