b5f3fcacc06ea72bea9273e5fabc7b28fd0950df
Raju10136 <108601999+Raju10136@users.noreply.github.com>
2024-09-27 15:10:15 +0530

    Update EFSideNav.tsx

--- src/EFSubLayout/EFSideNav.tsx
+++ src/EFSubLayout/EFSideNav.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 interface childrenProps {
   isOpen: boolean
 }
@@ -8,7 +9,7 @@ const EFSideNav: React.FC<childrenProps> = ({ isOpen }) => {
     {
       id: 1,
       label: "Dashboard",
-      icon:"fa-dashcube",
+      icon: "fa-dashcube",
       link: "/e-fuel/dashboard",
 
     },
@@ -16,49 +17,49 @@ const EFSideNav: React.FC<childrenProps> = ({ isOpen }) => {
     {
       id: 2,
       label: "Offices",
-      icon:"fa-cubes",
+      icon: "fa-cubes",
       link: "/e-fuel/offices-list",
 
     },
     {
       id: 3,
       label: "Hubs",
-      icon:"fa-cubes",
+      icon: "fa-cubes",
       link: "/e-fuel/hubs-list",
 
     },
     {
       id: 4,
       label: "Vendors",
-      icon:"fa-cubes",
+      icon: "fa-cubes",
       link: "/e-fuel/vendors-list",
 
     },
     {
       id: 5,
       label: "Vendor Rates",
-      icon:"fa-cubes",
+      icon: "fa-cubes",
       link: "/e-fuel/vendors-rates-list",
 
     },
     {
       id: 5,
       label: "Vehicles Report",
-      icon:"fa-cubes",
+      icon: "fa-cubes",
       link: "/e-fuel/vehicles-report",
 
     },
     {
       id: 6,
       label: "Consumption  Report",
-      icon:"fa-cubes",
+      icon: "fa-cubes",
       link: "/e-fuel/consumption-report",
 
     },
     {
       id: 7,
       label: "Invoices",
-      icon:"fa-cubes",
+      icon: "fa-cubes",
       link: "/e-fuel/invoices",
 
     },
@@ -66,41 +67,48 @@ const EFSideNav: React.FC<childrenProps> = ({ isOpen }) => {
     {
       id: 8,
       label: "Users",
-      icon:"fa-cubes",
+      icon: "fa-cubes",
       link: "/e-fuel/users",
 
     },
     {
       id: 8,
       label: "Roles",
-      icon:"fa-cubes",
+      icon: "fa-cubes",
       link: "/e-fuel/roles-list",
 
     },
     {
       id: 8,
       label: "Site Settings",
-      icon:"fa-cubes",
+      icon: "fa-cubes",
       link: "/e-fuel/settings",
 
     },
   ]
+  const navigate = useNavigate();
+
+  const navigateLink = (index: any) => {   
+    navigate(index);
+  };
+
+
   return (
     <div
       className={`smart-sidenav ${isOpen ? "expanded" : ""}`}
     >
-       {/* Brand Logo */}
-       <div className="brand-logo">
+      {/* Brand Logo */}
+      <div className="brand-logo">
         <img src={""} alt="Brand Logo" />
       </div>
-      {listItems.map((item,index)=>{
-        return ( 
-        <a href="#" className="sidenav-link">
-         <i className={`icon fa ${item.icon}`}></i>
-        <span className="link-text">{item.label}</span>
-      </a>
+      {listItems.map((item, index) => {
+        return (
+          <a key={`nav_link_${index}`} href="#" onClick={()=>navigateLink(item.link)} className="sidenav-link">
+            <i className={`icon fa ${item.icon}`}></i>
+            <span className="link-text">{item.label}</span>
+          </a>
         )
-      })}    
+      })}
     </div>
   );
 }
