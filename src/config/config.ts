a2f314e467c282e17e2db7b6acea620f0101e7e6
Raju10136 <mythri.raja@gmail.com>
2024-09-27 06:51:10 +0530

    ok

--- src/config/config.ts
+++ src/config/config.ts
@@ -3,14 +3,12 @@ import dev_config from "./developement";
 import prod_config from "./production";
 const env = process.env.REACT_APP_ENV || "default";
 
-let config:siteConfig;
-//console.log("env = ", env);
-if (env == "developement") {
+let config: siteConfig;
+if (env.trim() == "development") {
   config = dev_config;
-} else if (env == "production") {
+} else if (env.trim() == "production") {
   config = prod_config;
 } else {
   config = default_config;
 }
-
 export default config;
--- src/config/developement.ts
+++ src/config/developement.ts
@@ -1,5 +1,5 @@
-const config:siteConfig = {
-  REACT_APP_API_URL: "https://jsonplaceholder.typicode.com/",
+const config: siteConfig = {
+  REACT_APP_API_URL: "http://efl.softdigisolutions.com/api/",
   ENCRYPTION_KEY: "your-secret-key",
 };
 
