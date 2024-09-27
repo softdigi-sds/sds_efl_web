ee9bdd2b5eed87837783126daa5b36b47000a292
Janarthanan <139687402+jana050@users.noreply.github.com>
2024-09-27 10:12:59 +0530

    header upadte

--- src/EFSubLayout/EFHeader.tsx
+++ src/EFSubLayout/EFHeader.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { LOGO } from '../services/ImageService';
 import './EFSubLayout.css';
+import { useNavigate } from 'react-router-dom';
+import { useSiteContext } from '../contexts/SiteProvider';
 
 
 interface HeaderProps {
@@ -8,21 +10,114 @@ interface HeaderProps {
   isOpen:boolean
 }
 const EFHeader:React.FC<HeaderProps> = ({toggleSidebar,isOpen}) => {
+  const navigate = useNavigate();
+  const basenav = "/";
+  const { user, setUser, logout, setLoading } = useSiteContext();
+    const navigateLink = (index:any) => {
+  // navigate(basenav + "/" + index);
+  navigate(basenav + index);
+};
+const handleLogout = () => {
+  logout();
+  navigateLink("")
+};
+
+  const HeaderProfile = () => {
+  
+    return (
+      <>
+        <div className="ml-3 profile-button-dropdown dropdown   is-hoverable  pb-0 mt-1 mb-1 ">
+          <button className=" crop-business-laytout-top-button is-flex profile-hi-message-hover ">
+            <div className="business-profile-image  ">
+              {/* <SmartImageDisplay
+                srcType="DATA"
+                // url={img_url}
+                data={user?.profile_image}
+                // imageClass="is-48x48  is-rounded pointer"
+                // default_img={ADMIN_USER_LOGO}
+              /> */}
+            </div>
+            <div className="has-text-weight-bold ml-2 mt-1 is-flex ">
+              <div>Hi Admin</div>
+              <i className="fa fa-sort-desc ml-1 is-size-6 " aria-hidden="true"></i>
+            </div>
+          </button>
+          {/*         
+          <img
+            src={BUSSINESS_HEAD_TOP_RIGHT_lOGO}
+            alt="Business Head Top Right Logo"
+          /> */}
+          <div className="dropdown-menu dropdown-menu-margin mr-6">
+            <div className=" dropdown-content-profile pb-3 ">
+              <div className="dropdown-item p-0">
+                <div className="columns mb-0">
+                
+                
+                </div>
+                <div className="business-profile-border mt-0"></div>
+              
+
+                <div className="mt-3" key="g9">
+                  <ul className=" has-text-left profile-business-icon-page px-2">
+                    <li
+                      className="customer-profile-test pointer"
+                      onClick={() => navigateLink("home/my-profile-form")}
+                    >
+                      <span className="customer-home-icon-text-color">
+                        <i
+                          className="fa fa-user-circle-o"
+                          aria-hidden="true"
+                        ></i>
+                      </span>
+                      <span>My Profile</span>
+                    </li>
+                  
+                    <li
+                      className="customer-profile-test pointer"
+                      onClick={() => navigateLink("settings/pin-change-form")}
+                    >
+                      <span className="customer-home-icon-text-color">
+                        <i className="fa fa-cog" aria-hidden="true"></i>
+                      </span>
+                      <span>Settings</span>
+                    </li>
+                    <li
+                      className="customer-profile-test pointer"
+                      onClick={handleLogout}
+                    >
+                      <span className="customer-home-icon-text-color">
+                        <i className="fa fa-sign-out" aria-hidden="true"></i>
+                      </span>
+                      <span>Logout</span>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  };
+
+  
   return (
     <div>
-    <div className='columns'>
-    {isOpen&&<div className='column is-3' >
+    <div className='column is-flex is-justify-content-space-between'>
+    {isOpen&&<div className='' >
        <figure className="image  smart-el-header-icon-image">
               <img className="smart-side-nav-logo" src={LOGO} alt="" />
             </figure>
        </div>}
-       <div className='column is-1' >
+       <div className='is-flex' >
       <div  className='header-close-icon pointer'><i className="fa fa-bars" aria-hidden="true"></i></div> 
+      Electric Fuel
        </div>
      
-       <div className='column ' >
-       <i className="fa fa-bell has-text-right" aria-hidden="true"></i>
+       <div className=' ' >
+      {HeaderProfile()}
    </div>
+   
     </div>
        </div>
   )
