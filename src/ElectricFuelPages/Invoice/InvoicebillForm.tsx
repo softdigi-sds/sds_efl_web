d87a8bc3f9bdbf86f67829158e2012b11b4780ee
Janarthanan <139687402+jana050@users.noreply.github.com>
2024-10-03 10:32:51 +0530

    update

--- src/ElectricFuelPages/Invoice/InvoicebillForm.tsx
+++ src/ElectricFuelPages/Invoice/InvoicebillForm.tsx
@@ -19,15 +19,24 @@ const InvoicebillForm = () => {
 
   const handleInputChange = (name: string, value: any) => {
     if (name === "bill_start_date") {
-      // Parse the start date and add 1 day (or however many you want)
+     
       const startDate = new Date(value);
-      const minEndDate = new Date(startDate);
-      minEndDate.setDate(minEndDate.getDate() + 30); // Add 1 day
+      const newMinEndDate = new Date(startDate);
+      newMinEndDate.setDate(newMinEndDate.getDate() + 30); 
       
-      setMinEndDate(minEndDate); // Set the minimum end date
+      setMinEndDate(newMinEndDate); 
+      
+     
+      setFormData((prev: any) => ({
+        ...prev,
+        [name]: value,
+        bill_end_date: newMinEndDate, 
+      }));
+    } else {
+      setFormData((prev: any) => ({ ...prev, [name]: value }));
     }
-    setFormData((prev: any) => ({ ...prev, [name]: value }));
   };
+  
   const handleErrorChange = (name: string | any, value: any) => {
     setFormErrors((prev) => {
       const updatedFormData = { ...prev };
@@ -74,7 +83,7 @@ const InvoicebillForm = () => {
         placeHolder: "End Date",
         isRequired: true,
            minDate: minEndDate,
-        // inputProps: { isFocussed: true },
+         inputProps: { disabled: true},
       },
     },
     {
