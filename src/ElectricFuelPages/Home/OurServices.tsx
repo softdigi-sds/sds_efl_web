5e4b2fedf8dbf1b41c106b92eec1a4c20ac59ecc
pstamizh <pstamizhselvan@gmail.com>
2024-10-07 17:10:30 +0530

    update

--- src/ElectricFuelPages/Home/OurServices.tsx
+++ src/ElectricFuelPages/Home/OurServices.tsx
@@ -9,6 +9,7 @@ import {
   OUR_SERVICE_CARD_THREE,
   OUR_SERVICE_CARD_TWO,
 } from "../../services/ImageService";
+
 const OurServices = () => {
   useEffect(() => {
     Aos.init({
@@ -17,17 +18,22 @@ const OurServices = () => {
       mirror: false, // Whether elements should animate out while scrolling past them
     });
   }, []);
+
   return (
     <div className="our-services-container">
-      <p className="our-services-header-text">
+      <p className="our-services-header-text has-text-centered-mobile">
         Our <span>Services</span>
       </p>
       <div className="columns is-multiline">
-        <div className="column is-4">
+        <div className="column is-12-mobile is-6-tablet is-4-desktop">
           <div data-aos="fade-up" data-aos-anchor-placement="bottom-bottom">
             <div className="our-services-card-body">
               <div className="images">
-                <img src={OUR_SERVICE_CARD_ONE} alt="" />
+                <img
+                  src={OUR_SERVICE_CARD_ONE}
+                  alt="EV Charging Stations"
+                  className="is-fullwidth"
+                />
               </div>
               <div className="our-services-card-header">
                 EV Charging Stations
@@ -41,11 +47,16 @@ const OurServices = () => {
             </div>
           </div>
         </div>
-        <div className="column is-4">
+
+        <div className="column is-12-mobile is-6-tablet is-4-desktop">
           <div data-aos="fade-up" data-aos-anchor-placement="bottom-bottom">
             <div className="our-services-card-body">
               <div className="images">
-                <img src={OUR_SERVICE_CARD_TWO} alt="" />
+                <img
+                  src={OUR_SERVICE_CARD_TWO}
+                  alt="Corporate Charging Hubs"
+                  className="is-fullwidth"
+                />
               </div>
               <div className="our-services-card-header">
                 Corporate Charging Hubs
@@ -54,32 +65,42 @@ const OurServices = () => {
               <div className="our-services-card-content">
                 We provide Corporate Charging Hubs to B2B companies who are
                 serving e-commerce businesses for last mile delivery of products
-                within the city
+                within the city.
               </div>
             </div>
           </div>
         </div>
-        <div className="column is-4">
+
+        <div className="column is-12-mobile is-6-tablet is-4-desktop">
           <div data-aos="fade-up" data-aos-anchor-placement="bottom-bottom">
             <div className="our-services-card-body">
               <div className="images">
-                <img src={OUR_SERVICE_CARD_THREE} alt="" />
+                <img
+                  src={OUR_SERVICE_CARD_THREE}
+                  alt="Highway Charging"
+                  className="is-fullwidth"
+                />
               </div>
-              <div className="our-services-card-header">Highway charging</div>
+              <div className="our-services-card-header">Highway Charging</div>
               <div className="our-services-card-header-bar"></div>
               <div className="our-services-card-content">
                 We offer ultra fast charging to Electric vehicles along major
                 highway routes. Heavy-duty EV trucks require intelligent
-                high-power charging infrastructure
+                high-power charging infrastructure.
               </div>
             </div>
           </div>
         </div>
-        <div className="column is-4">
+
+        <div className="column is-12-mobile is-6-tablet is-4-desktop">
           <div data-aos="fade-up" data-aos-anchor-placement="bottom-bottom">
             <div className="our-services-card-body">
               <div className="images">
-                <img src={OUR_SERVICE_CARD_FOUR} alt="" />
+                <img
+                  src={OUR_SERVICE_CARD_FOUR}
+                  alt="Fleet Electrification"
+                  className="is-fullwidth"
+                />
               </div>
               <div className="our-services-card-header">
                 Fleet Electrification
@@ -87,40 +108,48 @@ const OurServices = () => {
               <div className="our-services-card-header-bar"></div>
               <div className="our-services-card-content">
                 We facilitate conversion of diesel vehicles into electric
-                through retro fitting thereby saving fleet operation cost upto
-                40%.
+                through retrofitting, saving fleet operation costs up to 40%.
               </div>
             </div>
           </div>
         </div>
-        <div className="column is-4">
+
+        <div className="column is-12-mobile is-6-tablet is-4-desktop">
           <div data-aos="fade-up" data-aos-anchor-placement="bottom-bottom">
             <div className="our-services-card-body">
               <div className="images">
-                <img src={OUR_SERVICE_CARD_FIVE} alt="" />
+                <img
+                  src={OUR_SERVICE_CARD_FIVE}
+                  alt="Residential Communities"
+                  className="is-fullwidth"
+                />
               </div>
               <div className="our-services-card-header">
                 Residential Communities
               </div>
               <div className="our-services-card-header-bar"></div>
               <div className="our-services-card-content">
-                With increased penetration of electrical vehicles for personal
-                use, we offer dedicated charging hubs for gated communities &
-                corporate parks
+                With the rise of electric vehicles for personal use, we offer
+                dedicated charging hubs for gated communities & corporate parks.
               </div>
             </div>
           </div>
         </div>
-        <div className="column is-4">
+
+        <div className="column is-12-mobile is-6-tablet is-4-desktop">
           <div data-aos="fade-up" data-aos-anchor-placement="bottom-bottom">
             <div className="our-services-card-body">
               <div className="images">
-                <img src={OUR_SERVICE_CARD_SIX} alt="" />
+                <img
+                  src={OUR_SERVICE_CARD_SIX}
+                  alt="Fleet Management"
+                  className="is-fullwidth"
+                />
               </div>
               <div className="our-services-card-header">Fleet Management</div>
               <div className="our-services-card-header-bar"></div>
               <div className="our-services-card-content">
-                Using the latest cloud based fleet management platform & mobile
+                Using the latest cloud-based fleet management platform & mobile
                 app, EFL offers Fleet Management Services for fleet and logistic
                 companies.
               </div>
