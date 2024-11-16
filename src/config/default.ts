dd8c623045a44d670d27fbbb0655f0f1a6f9d146
Raju10136 <mythri.raja@gmail.com>
2024-11-16 18:13:42 +0530

    production

--- src/config/default.ts
+++ src/config/default.ts
@@ -1,8 +1,6 @@
 const config: siteConfig = {
   // Softdigi endpoint
-  // REACT_APP_API_URL: "http://efl.softdigisolutions.com/api/",
-  REACT_APP_API_URL: "http://192.168.1.38/sds_efl_background/api",
-
+  REACT_APP_API_URL: "http://efl.softdigisolutions.com/api/",
 };
 
 export default config;
