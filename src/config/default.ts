42db4b94f4b9ead21b2af45d7b85aeaa79ad08a7
Raju10136 <mythri.raja@gmail.com>
2024-11-28 23:34:24 +0530

    production

--- src/config/default.ts
+++ src/config/default.ts
@@ -1,7 +1,7 @@
 const config: siteConfig = {
   // Softdigi endpoint
   REACT_APP_API_URL: "https://efl.softdigisolutions.com/api/",
-  DIGI_SERVER_URL: "http://smartpdf.softdigisolutions.com/",
+  DIGI_SERVER_URL: "https://sds.softdigisolutions.com/",
 };
 
 export default config;
