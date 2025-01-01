00d03246f5e2c25ff85e98643f2de82de10a11f8
Raju10136 <108601999+Raju10136@users.noreply.github.com>
2025-01-01 16:00:15 +0530

    deploy

--- src/config/production.ts
+++ src/config/production.ts
@@ -1,7 +1,6 @@
 const config:siteConfig = {
-    REACT_APP_API_URL: 'https://api.example.com',
-    ENCRYPTION_KEY : 'your-secret-key',
-    DIGI_SERVER_URL:"http://localhost:3000/"
+    REACT_APP_API_URL: 'https://git.tthefl.com/api',
+    DIGI_SERVER_URL: "https://sds.softdigisolutions.com/",
   };
 
   export default config;
