79ab982eb702ecbb79cb06067536a43fb063e8a9
Raju10136 <mythri.raja@gmail.com>
2025-01-02 20:04:20 +0530

    ok update

--- src/config/production.ts
+++ src/config/production.ts
@@ -1,6 +1,6 @@
-const config:siteConfig = {
-    REACT_APP_API_URL: 'https://git.tthefl.com/api',
-    DIGI_SERVER_URL: "https://sds.softdigisolutions.com/",
-  };
+const config: siteConfig = {
+  REACT_APP_API_URL: "https://ttlefl.com/api",
+  DIGI_SERVER_URL: "https://sds.softdigisolutions.com/",
+};
 
-  export default config;
+export default config;
